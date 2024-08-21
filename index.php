40b60da18e9e64c743b44b060d57d4f697ca9deb
William Gregorio <mail@williamgregorio.com>
2024-08-21 00:19:35 -0400

    boiler htm added

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php 
-echo('book')
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Hello, world!</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="description" content="" />
+  <link rel="icon" href="favicon.png">
+  <link rel="stylesheet" href="style.css" >
+</head>
+<body>
+  <h1>liberpdf</h1>
+</body>
+</html>
