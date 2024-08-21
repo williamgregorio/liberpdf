47106112a8dcb8e39e764d426f19eaa440d0e8c8
William Gregorio <mail@williamgregorio.com>
2024-08-21 07:00:24 -0400

    db conn init

--- register.php
+++ register.php
@@ -6,7 +6,15 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$dbHost = $_ENV['DB_HOST'];
+$DB_HOST = $_ENV['DB_HOST'];
+$DB_USER = $_ENV['DB_USER'];
+$DB_PASS = $_ENV['DB_PASS'];
+$DB_NAME = $_ENV['DB_NAME'];
+
+$conn = mysql_connect($DATABASE_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+if (mysql_connect_errno()) {
+  exit("Failed to connect " . mysql_connect_errno());
+}
 
 
-?>
+
