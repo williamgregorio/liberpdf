c089cdcc573bd4250c978f5d5f8bf14c9739dab5
William Gregorio <mail@williamgregorio.com>
2024-08-23 05:37:59 -0400

    on post connection init

--- register.php
+++ register.php
@@ -1,1 +1,11 @@
 <?php
+
+include 'db_connection.php';
+
+if ($_SERVER['REQUEST_METHOD']) == 'POST' {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $conn = openCon();
+
+}
