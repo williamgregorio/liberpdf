0830bc60a2ac1aacac99ec700ab2c445ae6d9b47
William Gregorio <mail@williamgregorio.com>
2024-08-23 05:44:14 -0400

    stmt addition to accounts

--- register.php
+++ register.php
@@ -3,9 +3,15 @@
 include 'db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD']) == 'POST' {
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+  $username = $_POST['username'];
+  $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $conn = openCon();
+  $conn = openCon();
+
+  $stmt = $conn->prepare('INSERT INTO accounts (username, password) VALUES (?,?)');
+  $stmt->bind_param('ss', $username, $password);
+
+
+    
 
 }
