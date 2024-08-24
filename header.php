3a5d65f2f0fd92aa563031ef5f6db51edcd508ca
William Gregorio <mail@williamgregorio.com>
2024-08-23 23:11:41 -0400

    statically added li a

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
   <ul>
     <li><a href="index.php">Home</a></li>
     <li><a href="register.php">Register</a></li>
+    <li><a href="login.php">Login</a></li>
   </ul>
 </nav>
 <div class="gcse-search"></div>
