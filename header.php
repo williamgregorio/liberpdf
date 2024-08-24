23b87bc75d1b9ec03245be4f3207672aa9758221
William Gregorio <mail@williamgregorio.com>
2024-08-23 23:35:28 -0400

    header auth check a user

--- header.php
+++ header.php
@@ -18,8 +18,17 @@
 <nav>
   <ul>
     <li><a href="index.php">Home</a></li>
-    <li><a href="register.php">Register</a></li>
-    <li><a href="login.php">Login</a></li>
+<?php
+    session_start();
+
+    if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] === true) {
+      echo '<li><a href="admin.php">Admin</a></li>';
+      echo '<li><a href="logout.php">Logout</a></li>';
+    } else {
+      echo '<li><a href="register.php">Register</a></li>';
+      echo '<li><a href="login.php">Login</a></li>';
+    }
+?>
   </ul>
 </nav>
 <div class="gcse-search"></div>
