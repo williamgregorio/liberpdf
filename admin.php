6f8098d46201946cf31aaa6bc1df941dd7687d59
William Gregorio <mail@williamgregorio.com>
2024-08-23 23:19:12 -0400

    restrcited page only to auth users

--- admin.php
+++ admin.php
@@ -1,4 +1,18 @@
 <?php
 session_start();
 
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+  header('Location: login.php');
+  exit();
+}
+
 ?>
+
+<?php
+$pageTitle = 'Admin';
+include 'header.php';
+?>
+
+<h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?></h1>
+
+<?php include 'footer.php' ?>
