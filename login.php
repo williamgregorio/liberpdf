0137c5452a0dacce7703160aab885f1add6cce7c
William Gregorio <mail@williamgregorio.com>
2024-08-23 22:53:39 -0400

    preparing sql look up and injections

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php 
+$pageTitle = "Login";
+include 'header.php';  
+?>
+
+<form method="post" action="">
+  Username: <input type="text" name="username" required><br>
+  Password: <input type="password" name="password" required><br>
+  <button type="submit">Login</button>
+</form>
+
+<?php include 'footer.php'; ?>
+
