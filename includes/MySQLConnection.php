5b1e193474072188a582f3289ddda5f2f554909b
William Gregorio <mail@williamgregorio.com>
2024-08-28 05:08:36 -0400

    inital connection

--- includes/MySQLConnection.php
+++ includes/MySQLConnection.php
@@ -8,5 +8,23 @@ class MySQLConnection {
   private $password;
   private $database;
 
-                        }
+  public function __construct($hostname, $username, $password, $database) {
+    $this->hostname = $hostname;
+    $this->username = $username;
+    $this->password = $password;
+    $this->database = $database;
+
+    $this->connect();
+  }
+
+  private function connect() {
+    $this->mysqli = new mysqli($this->hostname, $this->username, $this->password, $this->database);
+
+    if ($this->mysqli->connect_error) {
+      die('Connection Error (' . $this->mysqli->connect_errno . ') ' . $this->mysqli->connect_error);
+    }
+  }
+
 }
+
+?>
