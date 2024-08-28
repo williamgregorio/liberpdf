7001d48559063f9609bd34c03a50e434b8405dae
William Gregorio <mail@williamgregorio.com>
2024-08-28 05:49:22 -0400

    lets make sure we grab it

--- tests/MySQLConnectionTest.php
+++ tests/MySQLConnectionTest.php
@@ -1,5 +1,17 @@
 <?php
 
-use PHPUnit/Framework/TestCase;
+use PHPUnit\Framework\TestCase;
+use Dotenv\Dotenv;
 
 require 'includes/MySQLConnection.php';
+
+class MySQLConnectionTest extends TestCase {
+  private $db;
+
+  protected function setUp(): void {
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+  }
+
+
+}
