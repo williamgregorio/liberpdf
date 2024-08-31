8149e3ffd0149e5376be4ce8e52183c00fdf8f43
William Gregorio <mail@williamgregorio.com>
2024-08-31 05:38:14 -0400

    env load, and databse new conn

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+require_once("vendor/autoload.php");
+require_once 'includes/MySQLConnection.php';
+
+use Dotenv\Dotenv;
+
+function loadEnvironment() {
+  $dotenv = Dotenv::createImmutable(__DIR__);
+  return $dotenv->load();
+}
+
+function database() {
+  $hostname = $_ENV['DB_HOST'];
+  $username = $_ENV['DB_USER'];
+  $password = $_ENV['DB_PASS'];
+  $database = $_ENV['DB_NAME'];
+
+  $connection = new MySQLConnection($hostname, $username, $password, $database);
+  return $connection;
+}
+
