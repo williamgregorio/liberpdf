c1fe2392f06c282515ea9eb01d698d54823496b7
William Gregorio <mail@williamgregorio.com>
2024-09-21 00:43:35 -0400

    view all your books query

--- view-all-books.php
+++ view-all-books.php
@@ -13,3 +13,39 @@ isAuthenticated();
 
 $database = database();
 
+$user_id = $_SESSION['user_id'];
+
+$sql = 'SELECT books.title, books.author, books.url, categories.name AS category_name FROM books JOIN categories on books.category_id = categories.id WHERE categories.user_id = ?';
+$params = [$user_id];
+$types = 'i';
+
+$all_books = $database->query($sql, $params, $types);
+?>
+
+<h2>All Books</h2>
+<?php if ($all_books): ?>
+<table cellspacing="0" cellpadding="0" border="1">
+  <thead>
+    <tr>
+      <th>Title</th>
+      <th>Author</th>
+      <th>Category</th>
+      <th>URL</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($all_books as $book): ?>
+    <tr>
+      <td><?php echo htmlspecialchars($book['title']); ?></td>
+      <td><?php echo htmlspecialchars($book['author']); ?></td>
+      <td><?php echo htmlspecialchars($book['category_name']); ?></td>
+      <td><a href="<?php echo htmlspecialchars($book['url']); ?>" target="_blank">Link</a></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+<?php else: ?>
+<p>No books found for your account.</p>
+<?php endif; ?>
+
+<?php include 'footer.php'; ?>
