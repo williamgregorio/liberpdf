df5c7f0bea32648df6bd8f638c90ea88a2b2a993
William <mrwilliamgregorio@yahoo.com>
2024-10-04 16:21:53 -0400

    import router

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-
 <?php 
-$pageTitle = "Home";
-include 'templates/header.php';
-?>
-
-<?php include 'templates/footer.php'; ?>
+require 'router/main.php';
 
