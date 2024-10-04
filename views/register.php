ac230cecb0b6cc6cd660662c0cb92ab4eef099c2
William <mrwilliamgregorio@yahoo.com>
2024-10-04 19:04:09 -0400

    debu, will add 2nd paswd and && for uri

--- views/register.php
+++ views/register.php
@@ -1,7 +1,13 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 require 'templates/header.php';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  require '../middleware/db.php';
+if ($_SERVER['REQUEST_URI'] === '/register' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+  $root = dirname(__DIR__);
+  require $root . '/middleware/db.php';
 
   $username = $_POST['username'];
   $password = $_POST['password'];
@@ -9,6 +15,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   if (createUser($username,$password, $email)) {
     echo 'New account created successfully..redirect user please';
+    header('Location: /dashboard');
+    exit();
   } else {
    echo 'Account will not be created at this time.'; 
   }
@@ -16,8 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 <h1>Register</h1>
-
-<form method="POST" action="register.php">
+<form method="POST" action="/register">
   <input type="text" name="username" placeholder="Username" required>
   <input type="password" name="password" placeholder="Password" required>
   <input type="email" name="email" placeholder="Email" required>
