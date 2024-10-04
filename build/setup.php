dd8a49db3f01f17e04b4992fa60eba3f0aed9727
William <mrwilliamgregorio@yahoo.com>
2024-10-04 10:37:34 -0400

    r + w from sql

--- build/setup.php
+++ build/setup.php
@@ -1,5 +1,6 @@
 <?
 $db_path = __DIR__ . '../data/db.sqlite3';
+$tables_dir = __DIR__ . '../tables';
 
 if (file_exist($db_path)) {
   echo 'Database already exists in path.';
@@ -9,6 +10,11 @@ if (file_exist($db_path)) {
 try {
   $pdo = new PDO('sqlite:' . $db_path);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRORMODE_EXCEPTION);
+
+  function readAndExecuteSQL($pdo, $sql_file) {
+    $sql = file_get_contents($sql_file);
+    $pdo->exec($sql);
+  }
 
   echo 'Database created successfully.\n';
 } catch(PDOException $error) {
