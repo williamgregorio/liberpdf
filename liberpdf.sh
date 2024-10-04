20214a5583bd5e3999f4c22a09776eff35ba126e
William <mrwilliamgregorio@yahoo.com>
2024-10-04 12:33:17 -0400

    wht space and sql failure debg now

--- liberpdf.sh
+++ liberpdf.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 COMMAND="$1"
 DEFAULT_DATA_PATH="./data/db.sqlite3"
 DATA_DIR="./data"
 
 check_data_directory() {
   if [ ! -d "$DATA_DIR" ]; then
-    echo "Data directory does not exist.\n...Creating new directory"
+    printf "Data directory does not exist.\n...Creating new directory"
     mkdir -p "$DATA_DIR"
     echo "Directory 'data' has been created."
   fi
