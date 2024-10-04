9e06b1cdfefb2a23fa4fa56f8077fc2da7eb85da
William <mrwilliamgregorio@yahoo.com>
2024-10-04 17:15:23 -0400

    email uniqueness added

--- middleware/db.php
+++ middleware/db.php
@@ -1,8 +1,23 @@
 <?php
-
 function getConnection() {
   $db_path = dirname(__DIR__) . '/data/db.sqlite3';
   $pdo = new PDO('sqlite:' . $db_path);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   return $pdo;
 }
+
+function isEmailUnique($email) {
+  $pdo = getConnection(); 
+  $stmt = $pdo->prepare("SELECT COUNT(*) FROM users WHERE email = :email");
+  $stmt->bindParam(":email", $email);
+  $stmt->execute();
+  return $stmt->fetchColumn() === 0;
+}
+
+
+
+function createUser() {
+
+}
+
+
