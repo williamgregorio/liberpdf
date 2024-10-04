950c9bfe9d16b76349f30c657b488f8628b73571
William <mrwilliamgregorio@yahoo.com>
2024-10-04 19:52:42 -0400

    chng and mv on router req

--- views/register.php
+++ views/register.php
@@ -6,24 +6,6 @@ error_reporting(E_ALL);
 ?>
 <?php
 require 'templates/header.php';
-if ($_SERVER['REQUEST_URI'] === '/register' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-  $root = dirname(__DIR__);
-  require $root . '/middleware/db.php';
-
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-  $password2 = $_POST['password2'];
-  $email = $_POST['email'];
-
-  if (createUser($username,$password, $password2, $email)) {
-    echo 'New account created successfully.';
-    $_SESSION['username'] = $username;
-    header('Location: /dashboard');
-    exit;
-  } else {
-   echo 'Account will not be created at this time.'; 
-  }
-}
 ?>
 
 <h1>Create an account</h1>
