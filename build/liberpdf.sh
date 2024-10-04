b59740880d0b8ea84bc602c6136301ff8e8bfef6
William <mrwilliamgregorio@yahoo.com>
2024-10-04 10:58:17 -0400

    fi added, and status check on, sqlite3 must be installed first

--- build/liberpdf.sh
+++ build/liberpdf.sh
@@ -6,9 +6,18 @@ DEFAULT_DATA_PATH="../data/db.sqlite3"
 DATA_DIR= "../data"
 
 
-_check_data_directory() {
+check_data_directory() {
   if [! -d "$DATA_DIR"]; then
     echo "Data directory does not exist.\n...Creating new directory"
     mkdir -p "$DATA_DIR"
     echo "Directory 'data' has been created."
+  fi
 }
+
+check_db_status() {
+  if [[ -f "$DEFAULT_DATA_PATH" ]]; then
+    echo "Database 'db.sqlite3' file already exists."
+    
+  fi
+}
+
