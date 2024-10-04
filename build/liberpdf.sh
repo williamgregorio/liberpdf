6cc92794a3540626cc71114b07f7128434ed108e
William <mrwilliamgregorio@yahoo.com>
2024-10-04 10:53:34 -0400

    check me the dir

--- build/liberpdf.sh
+++ build/liberpdf.sh
@@ -5,3 +5,10 @@ COMMAND= "$1"
 DEFAULT_DATA_PATH="../data/db.sqlite3"
 DATA_DIR= "../data"
 
+
+_check_data_directory() {
+  if [! -d "$DATA_DIR"]; then
+    echo "Data directory does not exist.\n...Creating new directory"
+    mkdir -p "$DATA_DIR"
+    echo "Directory 'data' has been created."
+}
