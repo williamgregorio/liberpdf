ed3a46c1b871dc43a0a45c693832eee69d83743c
William <mrwilliamgregorio@yahoo.com>
2024-10-04 19:25:01 -0400

    dashboard redirection on session

--- views/admin.php
+++ views/admin.php
@@ -1,5 +1,11 @@
 <?php
-require 'templates/header.php';
+session_start();
+
+if (!isset($_SESSION['username'])) {
+  header('Location: /login');
+  exit;
+}
 ?>
 
+<?php require 'templates/header.php';?>
 <h1>Dashboard</h1>
