06aedf064bfe9aee8fb121d1e7e2bd3c15187469
William <mrwilliamgregorio@yahoo.com>
2024-10-04 10:39:30 -0400

    file declaration

--- build/setup.php
+++ build/setup.php
@@ -1,6 +1,6 @@
 <?
 $db_path = __DIR__ . '../data/db.sqlite3';
-$tables_dir = __DIR__ . '../tables';
+$tables_dir = __DIR__ . '../tables/';
 
 if (file_exist($db_path)) {
   echo 'Database already exists in path.';
@@ -15,6 +15,10 @@ try {
     $sql = file_get_contents($sql_file);
     $pdo->exec($sql);
   }
+
+  $user_sql_file = $tables_dir . 'user.sql';
+  $category_sql_file = $tables_dir . 'category.sql';
+  $book_sql_file = $tables_dir . 'book.sql';
 
   echo 'Database created successfully.\n';
 } catch(PDOException $error) {
