27b9f17cda680e2f1603f28472f6e023ae80202d
William <mrwilliamgregorio@yahoo.com>
2024-10-05 19:14:13 -0400

    create book form start

--- views/admin.php
+++ views/admin.php
@@ -2,9 +2,22 @@
 $pageTitle = 'Dashboard';
 require 'templates/header.php';
 ?>
+
 <h1>Dashboard, <?php echo $_SESSION['username'];?></h1>
 
-
+<h2>Create a new book</h2>
+<form method="POST" action="/create-book" style="">
+  <label for="title">Book title:</label>
+  <input type="text" name="title" required />
+  <br>
+  <label for="author">Author:</label>
+  <input type="text" name="author" required />
+  <br>
+  <label for="url">Book URL:</label>
+  <input type="url" name="url" required />
+</form>
 <?php
 //craete a new book form
 ?>
+
+<?php require 'templates/footer.php' ?>
