d1c88d61d620bdb82109b1ea953ca7cc15a753ea
William <mrwilliamgregorio@yahoo.com>
2024-10-05 17:00:42 -0400

    only set sess when called after php8

--- templates/header.php
+++ templates/header.php
@@ -6,21 +6,21 @@
   <meta name="description" content="" />
   <link rel="icon" href="favicon.png">
   <link rel="stylesheet" href="style.css" >
-<?php if (isset($pageTitle)) : ?>
-<title><?php echo htmlspecialchars($pageTitle, ENT_QUOTES, "UTF-8"); ?></title>
-<?php else : ?>
-  <title>Default Title</title>
-<?php endif; ?>
+  <?php if (isset($pageTitle)) : ?>
+  <title><?php echo htmlspecialchars($pageTitle, ENT_QUOTES, "UTF-8"); ?></title>
+  <?php else : ?>
+  <title>Default Title - Dev</title>
+  <?php endif; ?>
 </head>
 <body>
 <nav>
   <a href="/"><img src="assets/liberpdf-logo.png" alt="logo" /></a>
   <ul>
 <?php
-    if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] === true) {
-      echo '<li><a href="admin.php">Dashboard</a></li>';
-      echo '<li><a href="view-all-books.php">View all books</a></li>';
-      echo '<li><a href="logout.php">Logout</a></li>';
+    session_start();
+    if (isset($_SESSION['username'])) {
+      echo '<li><a href="/dashboard">Dashboard</a></li>';
+    echo '<li><a href="/logout">Logout</a></li>';
     } else {
       echo '<li><a href="/">Home</a></li>';
       echo '<li><a href="/register">Register</a></li>';
