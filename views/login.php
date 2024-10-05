f7a41e980d19f1414b160b587876c381d9172d41
William <mrwilliamgregorio@yahoo.com>
2024-10-04 20:05:08 -0400

    session will trns to middle

--- views/login.php
+++ views/login.php
@@ -1,5 +1,12 @@
 <?php
+session_start();
 include 'templates/header.php';
 ?>
 
 <h1>Login</h1>
+<form method="POST" action="/login">
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+</form>
+
