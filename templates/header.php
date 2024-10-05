9c18e55569627d812773e3bda5338f587e6231f7
William <mrwilliamgregorio@yahoo.com>
2024-10-05 17:03:50 -0400

    del on fav null for now

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <meta name="description" content="" />
-  <link rel="icon" href="favicon.png">
   <link rel="stylesheet" href="style.css" >
   <?php if (isset($pageTitle)) : ?>
   <title><?php echo htmlspecialchars($pageTitle, ENT_QUOTES, "UTF-8"); ?></title>
