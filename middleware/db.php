c336017f9698f6a6dbafc4d8ef61e51e1a8cd89e
William <mrwilliamgregorio@yahoo.com>
2024-10-13 03:07:22 -0400

    category id added to collect

--- middleware/db.php
+++ middleware/db.php
@@ -84,7 +84,7 @@ function checkUsernameById($username) {
   return $stmt->fetchColumn();
 }
 
-function createBook($username, $title, $author, $url) {
+function createBook($username, $category_id, $title, $author, $url) {
   $user_id = checkUsernameById($username);
   if (!$user_id) {
     echo 'username does not match this user id';
@@ -92,8 +92,9 @@ function createBook($username, $title, $author, $url) {
   }
 
   $pdo = getConnection();
-  $stmt = $pdo->prepare("INSERT INTO books (user_id, title, author, url) VALUES (:user_id, :title, :author, :url)");
+  $stmt = $pdo->prepare("INSERT INTO books (user_id, category_id, title, author, url) VALUES (:user_id, :category_id, :title, :author, :url)");
   $stmt->bindParam(':user_id', $user_id);
+  $stmt->bindParam(':category_id', $category_id);
   $stmt->bindParam(':title', $title);
   $stmt->bindParam(':author', $author);
   $stmt->bindParam(':url', $url);
